f67efd1b3d416c44572e72786d0a25c1347f7dfc
ghaniyyat05 <ghaniyyat@gmail.com>
2024-11-29 20:09:05 +0000

    Scanner - Standard Input value

---
+++ src/main/java/org/example/Scanner.java
@@ -0,0 +1,20 @@
+//Hackerrank 2nd question on java - input 3 values
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int a = scan.nextInt();
+        // Complete this line
+        int x = scan.nextInt();
+        int y = scan.nextInt();
+        scan.close();
+        
+
+        System.out.println(a);
+        // Complete this line    
+         System.out.println(x);
+         System.out.println(y);
+    }
+}
