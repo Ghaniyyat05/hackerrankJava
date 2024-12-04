75f702348325d438c69311f9516dd0d55eb64a6a
ghaniyyat05 <ghaniyyat@gmail.com>
2024-12-04 17:29:09 +0000

    Loops2

---
+++ src/main/java/org/example/LoopsII.java
@@ -0,0 +1,26 @@
+//Hackerrank 7th question 
+import java.util.*;
+import java.io.*;
+
+class Solution{
+    public static void main(String []argh){
+        Scanner in = new Scanner(System.in);
+        int t = in.nextInt();
+        
+        for(int i=0;i<t;i++){
+            
+            int a = in.nextInt();
+            int b = in.nextInt();
+            int n = in.nextInt();
+            
+         int currentValue = a;
+            for (int j = 0; j < n; j++) {
+                currentValue += (int) Math.pow(2, j) * b;
+                System.out.print(currentValue + " ");    
+        }
+         System.out.println();
+        }
+        
+        in.close();
+    }
+}
